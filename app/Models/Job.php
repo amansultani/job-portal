7c5f6c75376358d23719ff9913c3e533ba0e0d5e
amansultani <aman.sultani09@gmail.com>
2024-10-05 15:34:37 +0200

    Implementation of Employer model migration and relation

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,23 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Builder;   
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Job extends Model
 {
     /** @use HasFactory<\Database\Factories\JobFactory> */
     use HasFactory;
 
-    public static array $experience = ['entry','intermediate','senior'];
-    public static array $category = ['IT','Finance','Sales','Marketing'];
+    public static array $experience = ['entry', 'intermediate', 'senior'];
+    public static array $category = ['IT', 'Finance', 'Sales', 'Marketing'];
+
+    public function employer(): BelongsTo
+    {
+        return $this->belongsTo(Employer::class);
+    }
 
     public function scopeFilter(Builder|QueryBuilder $query, array $filters): Builder|QueryBuilder
     {
         return $query->when($filters['search'] ?? null, function ($query, $search) {
             $query->where(function ($query) use ($search) {
                 $query->where('title', 'like', '%' . $search . '%')
-                    ->orWhere('description', 'like', '%' . $search . '%');
+                    ->orWhere('description', 'like', '%' . $search . '%')
+                    ->orwhereHas('employer', function ($query) use ($search) {
+                        $query->where('company_name','like', '%' . $search . '%');
+                    });
             });
         })->when($filters['min_salary'] ?? null, function ($query, $minSalary) {
             $query->where('salary', '>=', $minSalary);
@@ -31,5 +40,5 @@ class Job extends Model
         })->when($filters['category'] ?? null, function ($query, $category) {
             $query->where('category', $category);
         });
-    }
-} 
+    } 
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+         User::factory(300)->create();
 
-        \App\Models\Job::factory(100)->create();
+        $users = User::all()->shuffle();
+
+        for( $i = 0; $i < 20; $i++ )
+        {
+            \App\Models\Employer::factory()->create( [
+                "user_id"=> $users->pop()->id
+            ]);
+        }
+
+            $employers = \App\Models\Employer::all();
+            for( $i = 0; $i < 100; $i++ )
+            {
+                \App\Models\Job::factory()->create( [
+                    'employer_id' => $employers->random()->id
+                ]);
+            }
+        
         // User::factory(10)->create();
 
         // User::factory()->create([
